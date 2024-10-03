a03585f699bbcf6cc6c2eaf2e630dc7b3c6b9d5d
Anjina Shah <shahanji247@gmail.com>
2024-10-03 19:14:06 -0400

    update dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { StrictMode } from "react";
+import "./global.css";
+import App from "./app/app";
+import Contact from "./contact/contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
